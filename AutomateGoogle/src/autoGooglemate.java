72add3beb66f3490fb39a0129615b5b5ad19b0c9
Tatenda Carl Sakarombe <90087474+Sakaz22@users.noreply.github.com>
2025-01-14 17:21:49 +0100

    Updated Chrome version

--- AutomateGoogle/src/autoGooglemate.java
+++ AutomateGoogle/src/autoGooglemate.java
@@ -1,30 +1,33 @@
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class autoGooglemate {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tcsak\\Downloads\\chromedriver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\tcsak\\Downloads\\chromedriver\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-		
-		ChromeDriver driver = new ChromeDriver();
-		
-		driver.get("http://www.google.com");
-		
-		WebElement searchBar = driver.findElement(By.name("q"));
-		
-		searchBar.sendKeys("Selenium Webdriver");
-		
-		searchBar.submit();
-		
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		driver.quit();
-	}
+        WebDriver driver = new ChromeDriver();
 
+        try {
+            driver.get("https://www.google.com");
+
+            // Explicit wait for the search bar to be interactable
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement searchBar = wait.until(ExpectedConditions.elementToBeClickable(By.name("q")));
+
+            searchBar.sendKeys("Selenium WebDriver");
+            searchBar.submit();
+
+            Thread.sleep(3000); // Optional: Wait to observe the search results
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
 }
